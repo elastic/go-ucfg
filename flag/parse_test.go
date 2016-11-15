c9d727d3a2991eca8114bb0bb365f3f01b7c1c06
urso <steffen.siering@elastic.co>
2016-11-15 17:16:25 +0100

    Enhance flag value parsing
    Enhance flag value parser to accept a superset of JSON, so arrays and objects
    can be configured from commandline
    
    The parser implements a superset of JSON, but only a subset of YAML by
    allowing for arrays and objects having a trailing comma. In addition 3
    strings types are supported:
    
    1. single quoted string (no unescaping of any characters)
    2. double quoted strings (characters are escaped)
    3. strings without quotes. String parsing stops in
      special characters like '[]{},:'

---
+++ flag/parse_test.go
@@ -0,0 +1,117 @@
+package flag
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlagValueParsing(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		// null
+		{"", nil},
+		{"null", nil},
+
+		// booleans
+		{`true`, true},
+		{`false`, false},
+		{`on`, true},
+		{`off`, false},
+
+		// unsigned numbers
+		{`23`, uint64(23)},
+
+		// negative number
+		{`-42`, int64(-42)},
+
+		// floating point
+		{`3.14`, float64(3.14)},
+
+		// strings
+		{`'single quoted'`, `single quoted`},
+		{`'single quoted \"'`, `single quoted \"`},
+		{`"double quoted"`, `double quoted`},
+		{`"double quoted \""`, `double quoted "`},
+		{`plain string`, `plain string`},
+
+		// test arrays
+		{`[]`, nil},
+		{
+			`[array, 1, true, "abc"]`,
+			[]interface{}{"array", uint64(1), true, "abc"},
+		},
+		{
+			`[test, [1,2,3], on]`,
+			[]interface{}{
+				"test",
+				[]interface{}{uint64(1), uint64(2), uint64(3)},
+				true,
+			},
+		},
+
+		// test dictionaries:
+		{`{}`, nil},
+		{`{'key1': true,
+       "key2": 1,
+       key 3: ['test', "test2", off],
+       nested key: {"a" : 2}}`,
+			map[string]interface{}{
+				"key1":  true,
+				"key2":  uint64(1),
+				"key 3": []interface{}{"test", "test2", false},
+				"nested key": map[string]interface{}{
+					"a": uint64(2),
+				},
+			},
+		},
+	}
+
+	for i, test := range tests {
+		t.Logf("run test (%v): %v", i, test.input)
+
+		v, err := parseValue(test.input)
+		if err != nil {
+			t.Error(err)
+			continue
+		}
+
+		assert.Equal(t, test.expected, v)
+	}
+
+}
+
+func TestFlagValueParsingFails(t *testing.T) {
+	tests := []string{
+		// strings:
+		`'abc`,
+		`"abc`,
+
+		// arrays
+		`[1,2,3`,         // missing ']'
+		`['abc' 'def']`,  // missing comma
+		`['abc', 'def,]`, // nested
+
+		// objects
+		`{a: 1, b:2`,      // missing '}'
+		`{'a' 1, b: 2}`,   // missing ':'
+		`{'a': '1' b: 2}`, // missing ','
+		`{'abc: 2}`,       // key parsing error
+		`{key: 'fail}`,    // value parsing error
+		`{:'abc'}`,        // object with missing key
+		`{nested: {a: 2}`, // nested object with missing '}'
+	}
+	for i, test := range tests {
+		t.Logf("run test(%v): %v", i, test)
+
+		_, err := parseValue(test)
+		if err == nil {
+			t.Errorf("parsing '%v' did not fail", test)
+			continue
+		}
+
+		t.Log("  Failed with: ", err.Error())
+	}
+}
