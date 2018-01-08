f313e1345c9b44232effce8299cca0bf4d026525
ph <phpellerin@gmail.com>
2018-01-08 10:24:55 -0500

    uses table test instead of two distinct tests

--- ucfg_test.go
+++ ucfg_test.go
@@ -7,61 +7,57 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var oneGraph = map[string]interface{}{
-	"n.a.b.c": "hello",
-	"n.a.d":   "world",
-	"values": []interface{}{
-		map[string]interface{}{
-			"j": "j-value",
-			"k": "k-value",
-		},
-		map[string]interface{}{
-			"j": "r-value",
-			"o": "o-value",
-		},
-	},
-}
-
 func TestFlattenKeys(t *testing.T) {
-	opts := []Option{PathSep(".")}
+	tests := []struct {
+		name    string
+		pathSep string
+	}{
+		{"withDot", "."},
+		{"emptySep", ""},
+	}
 
-	c, err := NewFrom(oneGraph, opts...)
-	assert.NoError(t, err)
+	sorted := func(s []string) []string {
+		sort.Strings(s)
+		return s
+	}
 
-	expected := []string{
+	cfg := map[string]interface{}{
+		"n.a.b.c": "hello",
+		"n.a.d":   "world",
+		"values": []interface{}{
+			map[string]interface{}{
+				"j": "j-value",
+				"k": "k-value",
+			},
+			map[string]interface{}{
+				"j": "r-value",
+				"o": "o-value",
+			},
+		},
+	}
+
+	expected := sorted([]string{
 		"n.a.b.c",
 		"n.a.d",
 		"values.0.j",
 		"values.0.k",
 		"values.1.j",
 		"values.1.o",
+	})
+
+	for _, test := range tests {
+		sep := test.pathSep
+		t.Run(test.name, func(t *testing.T) {
+			opts := []Option{}
+			if sep != "" {
+				opts = append(opts, PathSep(sep))
+			}
+
+			c, err := NewFrom(cfg, opts...)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, expected, sorted(c.FlattenedKeys(opts...)))
+		})
 	}
-	sort.Strings(expected)
-
-	results := c.FlattenedKeys(opts...)
-	sort.Strings(results)
-
-	assert.Equal(t, expected, results)
-}
-
-func TestFlattenKeysWithEmptyPathSep(t *testing.T) {
-	opts := []Option{}
-
-	c, err := NewFrom(oneGraph, opts...)
-	assert.NoError(t, err)
-
-	expected := []string{
-		"n.a.b.c",
-		"n.a.d",
-		"values.0.j",
-		"values.0.k",
-		"values.1.j",
-		"values.1.o",
-	}
-	sort.Strings(expected)
-
-	results := c.FlattenedKeys(opts...)
-	sort.Strings(results)
-
-	assert.Equal(t, expected, results)
 }
